3b5e42657d6022197fae8c43bda356c43f5df56e
Luckycarrot42 <eicarriemontecinos@gmail.com>
2020-08-19 22:55:17 -0400

    añadida notfound con boton

--- src/components/NotFound.vue
+++ src/components/NotFound.vue
@@ -1,12 +1,17 @@
 <template>
   <div>
     <h3>Error 404</h3>
+    <button @click="home">Volver al inicio</button>
   </div>
 </template>
 
 <script>
 export default {
-
+  methods:{
+    home(){
+      this.$router.push('/')
+    }
+  }
 }
 </script>
 
